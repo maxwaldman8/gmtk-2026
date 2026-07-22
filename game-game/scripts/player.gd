61c9bbe391b71470f736f2e5f0e51af1418e781e
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-22 12:53:37 -0700

    Test

--- game-game/scripts/player.gd
+++ game-game/scripts/player.gd
@@ -21,7 +21,6 @@ func _unhandled_input(event: InputEvent) -> void:
 			self.rotate_y(-event.relative.x * sensitivity)
 			camera.rotate_x(-event.relative.y * sensitivity)
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-50), deg_to_rad(60))
-			print_debug(camera.rotation.x)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
