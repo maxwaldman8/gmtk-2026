24ff0a574490d715fef8f39347bf6b66dc147d82
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-22 16:44:32 -0700

    Fixed

--- game-game/scripts/player.gd
+++ game-game/scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody3D
 
 
-@export var SPEED = 5.0
+@export var SPEED = 7.0
+@export var SPRINT_SPEED = 14.0
 @export var JUMP_VELOCITY = 4.5
 
 @export var sensitivity = 0.005
@@ -56,11 +57,19 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	if Input.is_action_pressed("sprint"):
+		if direction:
+			velocity.x = direction.x * SPRINT_SPEED
+			velocity.z = direction.z * SPRINT_SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPRINT_SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPRINT_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
