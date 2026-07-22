cf04eb84360385774e48dbda71374e27bf020bad
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-22 16:25:55 -0700

    test

--- game-game/scripts/screen/screen.gd
+++ game-game/scripts/screen/screen.gd
@@ -4,6 +4,10 @@ extends Control
 @export var player: CharacterBody3D
 @export var gui: CanvasLayer
 
+@export var time_label: Label
+
+var time: float = 48 * 60
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape_screen"):
 		player.process_mode = Node.PROCESS_MODE_ALWAYS
@@ -11,5 +15,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		get_parent().get_parent().visible = false
 		gui.visible = true
 
+func _process(delta: float) -> void:
+	time += delta
+	time_label.text = "11:" + floor(time / 60) + ":" + floor(time) % 60
+
+# window layering: clicked one is first
 # tween window with mouse
 # window pos offset for minimal overlap
