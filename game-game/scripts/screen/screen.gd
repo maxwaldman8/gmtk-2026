b75224cc3ab78707e2b4b7b3f4e753a2a2c7dce3
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-22 14:29:04 -0700

    Working focus system

--- game-game/scripts/screen/screen.gd
+++ game-game/scripts/screen/screen.gd
@@ -1,5 +1,13 @@
 class_name Screen
 extends Control
 
+@export var player: CharacterBody3D
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("escape_screen"):
+		player.process_mode = Node.PROCESS_MODE_ALWAYS
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		get_parent().get_parent().visible = false
+
 # window layering: clicked one is first
 # tween window with mouse
