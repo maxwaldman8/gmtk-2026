b72e6d10dec9012d7b6741087b6fa779e5a04180
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-22 22:44:16 -0700

    Added music

--- game-game/scripts/player.gd
+++ game-game/scripts/player.gd
@@ -37,6 +37,8 @@ func handle_raycast():
 				switch_to_screen()
 
 func switch_to_screen():
+	get_tree().root.get_node("Room").get_node("RealMusic").volume_db = -80
+	get_tree().root.get_node("Room").get_node("DigitalMusic").volume_db = 0
 	self.process_mode = Node.PROCESS_MODE_DISABLED
 	gui.visible = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -73,7 +75,3 @@ func _physics_process(delta: float) -> void:
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-
-func _on_screen_focus_exited() -> void:
-	pass # Replace with function body.
