442f00f493f23586e14be02ee7995641516b4130
knantamanasikarn1-commits <knantamanasikarn1@hwemail.com>
2026-07-24 21:31:41 -0700

    basic resizing

--- game-game/scripts/screen/browser websites/new_tab_website.gd
+++ game-game/scripts/screen/browser websites/new_tab_website.gd
@@ -1,14 +1,8 @@
 class_name NewTabWebsite
 extends BrowserWebsite
 
-@export var background : ColorRect
 static var bookmarked_websites : PackedStringArray = ["turn"]
 const url = ""
-
-
-func _ready() -> void:
-	background.size = Vector2(482, 481)
-	size = Vector2(482, 481)
 
 
 func get_url():
