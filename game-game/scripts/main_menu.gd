8c17e079d0988b21a5a798f82cae5ad5ef2dd152
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-25 21:14:49 -0700

    Music toggle

--- game-game/scripts/main_menu.gd
+++ game-game/scripts/main_menu.gd
@@ -3,8 +3,14 @@ extends Control
 func _on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/screen/screen_tutorial.tscn")
 
-func _on_settings_button_pressed() -> void:
-	pass
+func _on_music_button_pressed() -> void:
+	MusicSetting.music = !MusicSetting.music
+	if MusicSetting.music:
+		$MusicButton.text = "Music: ON"
+		$AudioStreamPlayer2D.volume_db = 0
+	else:
+		$MusicButton.text = "Music: OFF"
+		$AudioStreamPlayer2D.volume_db = -85
 
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
