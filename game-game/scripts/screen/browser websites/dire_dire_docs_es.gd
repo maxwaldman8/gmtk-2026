95cb4d95d1ae11c6f101d7f726aebf7db0c5a5fd
maxwaldman8 <39660554+maxwaldman8@users.noreply.github.com>
2026-07-25 18:08:42 -0700

    Fixed other dirediredocs

--- game-game/scripts/screen/browser websites/dire_dire_docs_es.gd
+++ game-game/scripts/screen/browser websites/dire_dire_docs_es.gd
@@ -11,11 +11,15 @@ func _on_log_in_1_button_pressed() -> void:
 	if $LoginPage1/PasswordField.text == get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().spanish_password:
 		$LoginPage1.visible = false
 		$LoginPage2.visible = true
+	else:
+		$LoginPage1/IncorrectPasswordLabel.visible = true
 
 func _on_log_in_2_button_pressed() -> void:
 	if $LoginPage2/SurnameField.text.to_lower() == get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().cat_surname.to_lower():
 		$LoginPage2.visible = false
 		$Essay.visible = true
+	else:
+		$LoginPage2/IncorrectPasswordLabel.visible = true
 
 func _on_download_button_pressed() -> void:
 	get_parent().get_parent().get_parent().get_parent().get_parent().get_parent().download_spanish_essay()
